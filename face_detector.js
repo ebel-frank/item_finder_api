8e016535dc84ab26083698acb7b97330f93073ff
ebel-frank <frankebeledike@gmail.com>
2024-09-12 01:12:03 +0100

    added verify face api

--- face_detector.js
+++ face_detector.js
@@ -5,20 +5,52 @@ const router = express.Router()
 const mongoose = require('mongoose');
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://mobile-biometric-voting-system-default-rtdb.firebaseio.com"
 });
+
+const embeddings = {}
+const threshold = 0.5
 
 const db = admin.firestore()
 async function loadFirestoreData() {
+    db.collection('face_embeddings').onSnapshot(async (snapshot) => {
+        snapshot.docChanges().forEach(async (change) => {
+            const userId = change.doc.id
 
+            if (change.type === 'added' || change.type === 'modified') {
+                embeddings[userId] = change.doc.data()
+            } else if (change.type === 'removed') {
+                embeddings.delete(userId)
+            }
+        })
+    })
 }
 
 
 loadFirestoreData()
 
 
-router.get('/api/verify_face', async  (req, res) => {
-    db.colllections('')
+router.get('/api/verify_face', async (req, res) => {
+    try {
+        const { predictedData, uid } = req.params
+        const minDist = 999;
+        const currDist = 0.0;
+        const userId = "";
+
+        print('users.length=> ${users.length}');
+
+        for (const uId in users.keys()) {
+            currDist = _euclideanDistance(users[uId], predictedData);
+            if (currDist <= threshold && currDist < minDist) {
+                minDist = currDist;
+                userId = uId;
+            }
+        }
+        res.status(200).json({'prediction': uid == userId}) // check if the userId of the predicted embeddings is same as actual user id
+    } catch (e) {
+
+    }
 })
 
 module.exports = router
