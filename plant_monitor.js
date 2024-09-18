f5453d3babe9c634789262f4338845b9737eff81
ebel-frank <frankebeledike@gmail.com>
2024-09-19 00:23:09 +0100

    hotfix

--- plant_monitor.js
+++ plant_monitor.js
@@ -16,12 +16,44 @@ router.get('/api/rain_state', async (req, res) => {
     }
   });
 
+  app.put('/api/rain_state', async (req, res) => {
+    const { rain_state } = req.body;
+    if (typeof rain_state !== 'boolean') {
+      return res.status(400).send({ message: 'Invalid value for rain_state' });
+    }
+  
+    try {
+      let state = await State.findOne();
+      state.rain_state = rain_state;
+      await state.save();
+      res.send({ rain_state: state.rain_state });
+    } catch (err) {
+      res.status(500).send({ message: 'Server error' });
+    }
+  });
+
   router.get('/api/pump_state', async (req, res) => {
     try {
       const state = await State.findOne(); // Assuming there's only one document
       if (!state) {
-        return res.status(404).send({ message: 'LED state not found' });
+        return res.status(404).send({ message: 'Soil data not found' });
       }
+      res.send({ pump_state: state.pump_state });
+    } catch (err) {
+      res.status(500).send({ message: 'Server error' });
+    }
+  });
+
+  app.put('/api/pump_state', async (req, res) => {
+    const { pump_state } = req.body;
+    if (typeof pump_state !== 'boolean') {
+      return res.status(400).send({ message: 'Invalid value for pump_state' });
+    }
+  
+    try {
+      let state = await State.findOne();
+      state.pump_state = pump_state;
+      await state.save();
       res.send({ pump_state: state.pump_state });
     } catch (err) {
       res.status(500).send({ message: 'Server error' });
