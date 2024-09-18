b06d093733b9a036849fa849e09742a37b1f3f08
ebel-frank <frankebeledike@gmail.com>
2024-09-19 00:09:15 +0100

    hotfix

--- plant_monitor.js
+++ plant_monitor.js
@@ -4,13 +4,13 @@ const Soil = require('./models/Soil')
 const State = require('./models/State')
 
 // GET route to retrieve led_state
-router.get('/api/led_state', async (req, res) => {
+router.get('/api/rain_state', async (req, res) => {
     try {
       const state = await State.findOne(); // Assuming there's only one document
       if (!state) {
         return res.status(404).send({ message: 'LED state not found' });
       }
-      res.send({ led_state: state.led_state });
+      res.send({ rain_state: state.rain_state });
     } catch (err) {
       res.status(500).send({ message: 'Server error' });
     }
