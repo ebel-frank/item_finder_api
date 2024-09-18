5a9f690b993e3c546d56c7309b67fa729b19e7a4
ebel-frank <frankebeledike@gmail.com>
2024-09-19 00:12:02 +0100

    hotfix

--- plant_monitor.js
+++ plant_monitor.js
@@ -16,4 +16,16 @@ router.get('/api/rain_state', async (req, res) => {
     }
   });
 
+  router.get('/api/pump_state', async (req, res) => {
+    try {
+      const state = await State.findOne(); // Assuming there's only one document
+      if (!state) {
+        return res.status(404).send({ message: 'LED state not found' });
+      }
+      res.send({ pump_state: state.pump_state });
+    } catch (err) {
+      res.status(500).send({ message: 'Server error' });
+    }
+  });
+
 module.exports = router
