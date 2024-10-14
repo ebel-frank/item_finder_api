85c28691650a0f444a884ffc371dabbda4f398fe
Frank C. Ebeledike <frankcebeledike@gmail.com>
2024-10-14 01:29:01 +0100

    hotfix

--- rescuer.js
+++ rescuer.js
@@ -79,34 +79,46 @@ Please act immediately.`
             console.log('Email sent:', info.response);
             res.status(200).json({ message: 'Email sent successfully!' });
         });
-    // } else {
-    //     const client = require('twilio')(process.env.TWILLIO_SID, process.env.AUTH_TOKEN);
+    } else {
+        const client = require('twilio')(process.env.TWILLIO_SID, process.env.AUTH_TOKEN);
 
-    //     client.messages
-    //         .create({
-    //             body: msg,
-    //             from: '+19292961968',
-    //             to: convertPhoneNumber(e_phone)
-    //         })
-    //         .then(message => {
-    //             console.log(message.sid)
-    //         });
+        client.messages
+            .create({
+                body: msg,
+                from: '+2349061353065',
+                to: convertPhoneNumber(e_phone)
+            })
+            .then(message => {
+                console.log(message.sid)
+            });
 
-    //     const mailOptions = {
-    //         from: 'businessrevolutionaries@gmail.com',  // Sender address
-    //         to: e_email,  // Recipient's email address (from request body)
-    //         subject: "URGENT: EMERGENCY ALERT",
-    //         text: msg
-    //     };
-
-    //     // Send the email using Nodemailer
-    //     transporter.sendMail(mailOptions, (error, info) => {
-    //         if (error) {
-    //             console.error('Error sending email:', error);
-    //         }
-    //         console.log('Email sent:', info.response);
-    //     });
-    //     res.status(200).json({ message: 'Both Email and SMS sent successfully!' });
+            const mailOptions = {
+                from: 'businessrevolutionaries@gmail.com',  // Sender address
+                to: e_email,  // Recipient's email address (from request body)
+                subject: "URGENT: EMERGENCY ALERT",
+                text: msg,
+                attachments: [
+                    {
+                      filename: userPhoto.originalname,
+                      content: userPhoto.buffer,
+                    },
+                  ],
+            };
+    
+            // Send the email using Nodemailer
+            transporter.sendMail(mailOptions, (error, info) => {
+                // Delete the image after sending the email
+                fs.unlink(image.path, (err) => {
+                    if (err) console.error('Failed to delete uploaded image:', err);
+                });
+    
+                if (error) {
+                    console.error('Error sending email:', error);
+                    return res.status(500).json({ message: 'Error sending email', error });
+                }
+                console.log('Email sent:', info.response);
+            });
+        res.status(200).json({ message: 'Both Email and SMS sent successfully!' });
     }
 });
 
