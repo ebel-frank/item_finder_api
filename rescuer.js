53e3d8f52aa176051c33f75555abbacbfac55841
Frank C. Ebeledike <frankcebeledike@gmail.com>
2024-10-14 01:18:44 +0100

    hotfix

--- rescuer.js
+++ rescuer.js
@@ -25,7 +25,7 @@ function convertPhoneNumber(number) {
 
 // API endpoint to send an email
 router.post('/api/alert-contacts', upload.single('image'), (req, res) => {
-    const { e_email, e_phone, e_name, user_name, e_relshp, lat, long, type } = req.body.json;
+    const { e_email, e_phone, e_name, user_name, e_relshp, lat, long, type } = JSON.parse(req.body.json)
     console.log(`${e_email} ${e_phone} ${e_name} ${user_name} ${e_relshp} ${lat} ${long} ${type}`)
     // console.log(json)
     res.status(200).json({ message: 'SMS sent!' });
